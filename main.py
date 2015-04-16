6a367e947c848f6bea8e6f561098bcfe817987fd
Anita Garcia <anitagarcia819@gmail.com>
2015-04-16 11:43:10 -0700

    Added a main and a TagSearch file.

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-from instagram.client import InstagramAPI
-import httplib2
-import json
-import urllib
-from  urllib import urlopen
-import csv
-
-tag_here = raw_input("Enter Tag: ")
-file = urlopen("https://api.instagram.com/v1/tags/"+ tag_here+"/media/recent?access_token=231920771.7f67456.bfe24e8a256d4d5ca2e2131d56b7103b").read()
-data = json.loads(file)
-csv = csv.writer(open("test.csv", "wb"))
-
-for data in data["data"]:
-	if(data['location'] != None):
-		variable = data['location'] #to test
-		print variable
-		csv.writerow([variable])
+# File name: main.py 
+# Authors: Anita & Gabriel Radinsky
+# Created: 4/2015
+# Description: This program will run the 
+# InstaDataViz program. It will allow 
+# the user select which feature 
+# of the program the user would like to select. 
