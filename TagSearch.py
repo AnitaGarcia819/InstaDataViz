53f972de7b7089a90f26b86734f5ccd63c4f4de5
Anita Garcia <anitagarcia819@gmail.com>
2015-04-20 15:00:45 -0700

    imported requests

--- TagSearch.py
+++ TagSearch.py
@@ -9,20 +9,20 @@
 from instagram.client import InstagramAPI
 import httplib2
 import json
-import urllib
-from  urllib import urlopen
-import csv
+import requests
+
 
 def tagSearch():
 	access_token = "231920771.7f67456.bfe24e8a256d4d5ca2e2131d56b7103b"
 	tag_here = raw_input("Enter First tag: ")
-	file = request.get("https://api.instagram.com/v1/tags/" +tag_name + "/media/recent?access_token=" + access_token).read()
+	file = request.get("https://api.instagram.com/v1/tags/" +tag_name + "/media/recent?access_token=" + access_token)
 	data = file.json()
 	#jasonfile = json.loads(file)
 
 
-	for x in data['location']:
+	for x in data['data']:
 		if(data['location'] != None):
 			variable = data['location']['latitude'] #to test
 			print variable
 			#csv.writerow([variable])
+tagSearch()
