418f4ea80e42f72dbf1ff08bcd2efdf30598c975
Anita Garcia <anitagarcia819@gmail.com>
2015-04-22 16:31:08 -0700

    code to save urls as files

--- PopularPictures.py
+++ PopularPictures.py
@@ -1,5 +1,21 @@
 from instagram.client import InstagramAPI
+from Tkinter import *        
+from PIL import ImageTk, Image
+import urllib
+
+
 api = InstagramAPI(client_id='7f674565db0d42ed9b4dd4f366db65be', client_secret='9a5f5c38eec74eff9f479e3659d4824f')
 popular_media = api.media_popular(count=20)
+i = 0
 for media in popular_media:
-    print media.images['standard_resolution'].url
+    url =  media.images['standard_resolution'].url
+    urllib.urlretrieve(url, "file"+ str(i) +".jpg")
+    i += 1
+
+#Setting it up
+img = ImageTk.PhotoImage(Image.open("file"+ str(i) +".jpg"))
+
+#Displaying it
+imglabel = Label(app_root, image=img).grid(row=1, column=1)        
+
+app_root.mainloop()
