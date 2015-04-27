1fec946de8fedb20b66a9bf753305c7461122477
Anita Garcia <anitagarcia819@gmail.com>
2015-04-27 14:10:34 -0700

    Added Word Cloud.py

--- PopularPictures.py
+++ PopularPictures.py
@@ -3,17 +3,17 @@ from Tkinter import *
 from PIL import ImageTk, Image
 import urllib
 
-
 api = InstagramAPI(client_id='7f674565db0d42ed9b4dd4f366db65be', client_secret='9a5f5c38eec74eff9f479e3659d4824f')
-popular_media = api.media_popular(count=20)
+popular_media = api.media_popular(count=1)
 i = 0
 for media in popular_media:
     url =  media.images['standard_resolution'].url
     urllib.urlretrieve(url, "file"+ str(i) +".jpg")
-    i += 1
+    i += 1 
+
 
 #Setting it up
-img = ImageTk.PhotoImage(Image.open("file"+ str(i) +".jpg"))
+img = ImageTk.PhotoImage(Image.open("file"+ str(0) +".jpg"))
 
 #Displaying it
 imglabel = Label(app_root, image=img).grid(row=1, column=1)        
