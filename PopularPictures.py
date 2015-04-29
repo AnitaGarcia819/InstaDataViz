58f92959fb738a83c62fd79e221c56102e1168e4
Anita Garcia <anitagarcia819@gmail.com>
2015-04-29 14:18:02 -0700

    deleted Tkinter

--- PopularPictures.py
+++ PopularPictures.py
@@ -1,6 +1,5 @@
 from instagram.client import InstagramAPI
-from Tkinter import *        
-from PIL import ImageTk, Image
+
 import urllib
 
 api = InstagramAPI(client_id='7f674565db0d42ed9b4dd4f366db65be', client_secret='9a5f5c38eec74eff9f479e3659d4824f')
@@ -12,10 +11,7 @@ for media in popular_media:
     i += 1 
 
 
-#Setting it up
-img = ImageTk.PhotoImage(Image.open("file"+ str(0) +".jpg"))
 
-#Displaying it
-imglabel = Label(app_root, image=img).grid(row=1, column=1)        
+       
 
-app_root.mainloop()
+
