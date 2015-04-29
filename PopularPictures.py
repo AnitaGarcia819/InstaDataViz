025e37ab9718427f9322a7615a67dee785654f2c
Anita Garcia <anitagarcia819@gmail.com>
2015-04-29 14:09:07 -0700

    Added Insta Tags into WordCloud

--- PopularPictures.py
+++ PopularPictures.py
@@ -4,7 +4,7 @@ from PIL import ImageTk, Image
 import urllib
 
 api = InstagramAPI(client_id='7f674565db0d42ed9b4dd4f366db65be', client_secret='9a5f5c38eec74eff9f479e3659d4824f')
-popular_media = api.media_popular(count=1)
+popular_media = api.media_popular(count=20)
 i = 0
 for media in popular_media:
     url =  media.images['standard_resolution'].url
--- WordCloud.py
+++ WordCloud.py
@@ -1,52 +1,69 @@
 from pytagcloud import create_tag_image, make_tags
 from pytagcloud.lang.counter import get_tag_counts
- 
-TEXT = '''
-You know the day destroys the night
-Night divides the day
-Tried to run
-Tried to hide
-Break on through to the other side
-Break on through to the other side
-Break on through to the other side, yeah
- 
-We chased our pleasures here
-Dug our treasures there
-But can you still recall
-The time we cried
-Break on through to the other side
-Break on through to the other side
-Yeah!
-C'mon, yeah
-Everybody loves my baby
-Everybody loves my baby
-She get
-She get
-She get
-She get high
-I found an island in your arms
-Country in your eyes
-Arms that chain us
-Eyes that lie
-Break on through to the other side
-Break on through to the other side
-Break on through, oww!
-Oh, yeah!
-Made the scene
-Week to week
-Day to day
-Hour to hour
-The gate is straight
-Deep and wide
-Break on through to the other side
-Break on through to the other side
-Break on through
-Break on through
-Break on through
-Break on through
-Yeah, yeah, yeah, yeah
-Yeah, yeah, yeah, yeah, yeah'''
- 
-tags = make_tags(get_tag_counts(TEXT), maxsize=150)
+from instagram.client import InstagramAPI
+import httplib2
+import json
+import urllib
+from  urllib import urlopen
+import time
 
-create_tag_image(tags, 'cloud_large.png', size=(900, 600))
+
+
+def getCityLatandLng(choice):
+	if choice == 1: #LA
+		lat = 33
+		lng = 118
+
+	elif choice == 2: #NYC
+		lat = 40
+		lng = 73
+
+	elif choice == 3: #SF
+		lat = 37
+		lng = 122
+
+	return lat, lng
+
+def storesTags(lat, lng):  
+    file = urlopen("https://api.instagram.com/v1/media/search?lat="+str(lat)+"&lng="+str(lng)+"&access_token=231920771.7f67456.bfe24e8a256d4d5ca2e2131d56b7103b").read()
+    data = json.loads(file)
+
+    tagsTempArray = []
+
+    for data in data["data"]:
+        if data ['tags'] != '':
+          	tag = data['tags']
+          	tagsTempArray.append(tag)
+    
+    tagsArray = []
+    for element in tagsTempArray:
+    	for tag in element:
+    		tagsArray.append(tag)
+    string = ' '.join(tagsArray)
+    return string
+
+
+"""
+YOUR_TEXT = "A tag cloud is a visual representation for text data, typically\
+used to depict keyword metadata on websites, or to visualize free form text."
+"""
+YOUR_TEXT = storesTags(37,122)
+tags = make_tags(get_tag_counts(YOUR_TEXT), maxsize=80)
+
+create_tag_image(tags, 'cloud_large.png', size=(900, 600), fontname='Lobster')
+
+import webbrowser
+webbrowser.open('cloud_large.png') # see results
+
+
+
+
+
+
+
+
+
+
+
+
+
