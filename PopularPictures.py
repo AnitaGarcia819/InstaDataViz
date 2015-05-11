531374bc962706053f2b8595a1af4de17b231d3e
Gabriel R <gradinsky@csumb.edu>
2015-05-11 14:32:50 -0700

    Comments

--- PopularPictures.py
+++ PopularPictures.py
@@ -1,13 +1,12 @@
-#  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # File name: PopularPictures.py
-# Authors: Anita & Gabriel Radinsky
+# Authors: Anita Garcia & Gabriel Radinsky
 # Created: 4/2015
-# Description: This program will run the entire program. A Graphical User Interface
-#              (GUI) will be used to display the options available to the user. 
-#              The user will select which feature of the program he/she wishes to run
-#              via the list of buttons.  
-#          
-#  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Description: This program queries the Instagram API to retrieve currently trending
+#              pictures, store them in .jpg format, and then upload them to a web
+#              server. After doing that, it opens the default browser to the webpage
+#
+#  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 from instagram.client import InstagramAPI
 import urllib
 import os
@@ -15,19 +14,23 @@ import platform
 
 system = platform.system()
 
+#Queries the API to get the currently trending images
 def createImageFiles():
 	api = InstagramAPI(client_id='7f674565db0d42ed9b4dd4f366db65be', client_secret='9a5f5c38eec74eff9f479e3659d4824f')
 	popular_media = api.media_popular(count=20)
 	i = 0
+	#Stores the pictures into .jpg format, in an easy to access order
 	for media in popular_media:
 		url =  media.images['standard_resolution'].url
 		urllib.urlretrieve(url, "file"+ str(i) +".jpg")
 		i += 1
 
+#Uploads all pictures to a remote webhost and removes all previous pictures
 def uploadPhotos():
 	for x in range(20):
 		os.system("scp file" + str(x) + ".jpg gradinsk@radinsky.me:/var/www/html/205/img/")
 
+#Opens a a new tab in the default browser to display the webpage
 def openWebPage():
 	if(system == "Darwin"):
 		os.system("open http://radinsky.me/205")
